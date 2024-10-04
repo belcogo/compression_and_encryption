9ad4760deb7f9ccb095775c363ee22a66d0c4a75
JoaoAccorsi <joaovaccorci@gmail.com>
2024-10-04 14:39:24 -0300

    Feat: Enhance Elias Gamma Decoder

--- src/eliasgamma.py
+++ src/eliasgamma.py
@@ -1,4 +1,3 @@
-import numpy as np
 import math
 from src.parser import Parser
 
@@ -8,11 +7,11 @@ class EliasGamma:
 
     def encoder(self, message_encoder : list):
         message_ascii = self.parser.parse_symbols_to_ascii(message_encoder)
-        print(message_ascii)
         i = 0
         final_code_word = ""
 
-        for number in message_ascii:
+        for _ in message_ascii:
+
             # Acha a potência de 2 mais próxima do número
             lower_power = 2 ** math.floor(math.log2(message_ascii[i]))  # Potência de 2 menor ou igual a n
         
@@ -21,7 +20,7 @@ class EliasGamma:
             
             # Formata a sequência de bits
             prefix = power_of_two(lower_power) * '0'
-            sufix = bin(rest)[2:]
+            sufix = bin(rest)[2:] + '1' # Adicionar o stop bit
 
             # Sequência de bits do símbolo e da palavra toda
             codeword = prefix + sufix
@@ -32,35 +31,35 @@ class EliasGamma:
     
 
     def decoder(self, message_decoder : list):
-
         decoded_numbers = []
         i = 0
     
         while i < len(message_decoder):
-            print(message_decoder)
-            # Step 1: Contar o número de zeros até o primeiro '1' (Prefixo Unário)
+
+            # Conta o número de zeros até o primeiro 1
             zero_count = 0
             while i < len(message_decoder) and message_decoder[i] == '0':
                 zero_count += 1
                 i += 1
         
-            # O próximo bit deve ser '1' (parte final do prefixo)
-            i += 1  # Avança para o próximo bit após o '1'
+            # Avança para o próximo bit depois do 1
+            i += 1 
         
-            # Step 2: Ler os próximos (zero_count) bits como o sufixo binário
+            # Ler o restante da codeword
             binary_number = '1'  # Começa com o bit '1' que foi omitido na codificação
             for _ in range(zero_count):
-                print(i)
-                binary_number += message_decoder[i-1]
-                i += 1
-        
-            # Step 3: Converter o número binário em decimal e adicionar à lista de números decodificados
-            decoded_numbers.append(int(binary_number, 2))
-            final = self.parser.parse_ascii_to_symbols(decoded_numbers)
+                if i < len(message_decoder):
+                    binary_number += message_decoder[i-1]
+                    i += 1
 
-            # message_ascii = self.parser.parse_symbols_to_ascii(message_encoder)
+            decoded_number = int(binary_number, 2)
 
-        return final
+            # Ver se o caractere está dentro do range da tabela ASCII
+            if 32 <= decoded_number <= 126:
+                decoded_numbers.append(int(binary_number, 2))
+                final = self.parser.parse_ascii_to_symbols(decoded_numbers)
+                        
+        return  "".join(value for value in final)
 
 
 def power_of_two(n):
